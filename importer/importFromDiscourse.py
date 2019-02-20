92bf3bef0405719562cc0f561ecfb2dc3e3bb2d6
Hugi <hugiasgeirsson@gmail.com>
2019-02-20 19:34:53 +0100

    replaced resulterror with sessionerror

--- importer/importFromDiscourse.py
+++ importer/importFromDiscourse.py
@@ -5,7 +5,7 @@ import string
 from datetime import datetime
 from py2neo import *
 from connector.neo4j import query_neo4j
-from neo4j.v1 import ResultError
+from neo4j.v1 import SessionError
 import requests
 import sys
 import re
--- routes/annotation/annotation_getter.py
+++ routes/annotation/annotation_getter.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from neo4j.v1 import ResultError
+from neo4j.v1 import SessionError
 from connector import neo4j
 from routes.utils import addargs, addTimeFilter, makeResponse
 
