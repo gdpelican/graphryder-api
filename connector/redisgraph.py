49de598a3f22316f84437beaaeeddb3dc09bede3
James <james.kiesel@gmail.com>
2020-06-12 10:47:36 +1200

    A better-faith guess at the redisgraph connector

--- connector/redisgraph.py
+++ connector/redisgraph.py
@@ -3,9 +3,14 @@ import configparser
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-def query_redisgraph(request):
-    result = requests.get(user_url, headers={
-        'User-Api-Key': config['discourse']['admin_api_key']
-    })
+def query_redisgraph(query):
+    result = requests.get(
+        f'{config['discourse']['url']}/graphryder/query',
+        headers={
+            'Content-Type': 'application/json',
+            'User-Api-Key': config['discourse']['admin_api_key'],
+        },
+        body={'query': query}
+    )
     print(result)
     return result
