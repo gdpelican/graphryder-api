1d0da4090aa7c658b980f5940f24c96ebf3838f3
James <james.kiesel@gmail.com>
2020-06-16 13:44:56 +1200

    Further rename post -> topic

--- routes/topic/topic_routes.py
+++ routes/topic/topic_routes.py
@@ -1,25 +1,23 @@
-from routes.post.post_getter import *
-from routes.post.post_aggregations import *
+from routes.topic.topic_getter import *
+from routes.topic.topic_aggregations import *
 
 
 def add_post_routes(api):
     # Getters
     # Multiple
-    api.add_resource(GetPosts, '/posts')
-    api.add_resource(GetPostsLatest, '/posts/latest')
-    api.add_resource(GetPostsByType, '/posts/type/<string:post_type>')
-    api.add_resource(GetPostsByAuthor, '/posts/author/<int:author_id>')
+    api.add_resource(GetTopics, '/posts')
+    api.add_resource(GetTopicsLatest, '/posts/latest')
+    api.add_resource(GetTopicsByType, '/posts/type/<string:post_type>')
+    api.add_resource(GetTopicsByAuthor, '/posts/author/<int:author_id>')
     # Single
-    api.add_resource(GetPost, '/post/<int:post_id>')
-    api.add_resource(GetPostHydrate, '/post/hydrate/<int:post_id>')
+    api.add_resource(GetTopic, '/post/<int:post_id>')
+    api.add_resource(GetTopicHydrate, '/post/hydrate/<int:post_id>')
     # Distinct
-    api.add_resource(GetPostType, '/post/getType')
+    api.add_resource(GetTopicType, '/post/getType')
 
     # Count
-    api.add_resource(CountAllPost, '/posts/count/')
-    api.add_resource(CountPostByAuthor, '/posts/count/author/<int:author_id>')
-    api.add_resource(CountPostsByTimestamp, '/posts/count/timestamp')
+    api.add_resource(CountAllTopic, '/posts/count/')
+    api.add_resource(CountTopicByAuthor, '/posts/count/author/<int:author_id>')
+    api.add_resource(CountTopicsByTimestamp, '/posts/count/timestamp')
 
-    # todo GetPostsByDate(min, max) need to have a fix time format
-
-
+    # todo GetTopicsByDate(min, max) need to have a fix time format
